d9c232077506df576c9357f6c04b64319ea2246f
Andrés Javier López <ajavier.lopez@gmail.com>
2016-11-17 15:47:56 -0600

    Agrega utf8 como charset por defecto para la conexión con MySQL

--- include/database/mysql/class.mysqlconnection.php
+++ include/database/mysql/class.mysqlconnection.php
@@ -9,37 +9,37 @@
  */
 
 class MySQLConnection implements DBConnection{
-	
+
 	/**
 	 * Recurso de la conexión
 	 * @var mysqli $connection
 	 */
 	protected $connection;
-	
+
 	/**
 	 * Servidor de base de datos
 	 * @var string $host
 	 */
 	protected $host;
-	
+
 	/**
 	 * Usuario de la base de datos
 	 * @var string $user
 	 */
 	protected $user;
-	
+
 	/**
 	 * Contraseña de la base de datos
 	 * @var string $password
 	 */
 	protected $password;
-	
+
 	/**
 	 * Nombre de la base de datos
 	 * @var string $database
 	 */
 	protected $database;
-	
+
 	/**
 	 * Inicializa el objeto de conexión
 	 * @param string $host
@@ -58,32 +58,33 @@ class MySQLConnection implements DBConnection{
 		$this->database = $database;
 		$this->defaultConnection();
 	}
-	
+
 	/**
 	 * Conecta con la base de datos
 	 * @return void
 	 * @throws BadConnectionException
-	 */	
+	 */
 	private function defaultConnection()
 	{
 		if(is_null($this->connection)) {
 			throw new BadConnectionException(_('No se ha iniciado la conexión con la base de datos'));
 		}
-	
+
 		$this->connection->real_connect( $this->host, $this->user, $this->password, $this->database );
+		$this->connection->set_charset('utf8');
 		if ($this->connection->connect_errno)
 		{
 			throw new BadConnectionException(_('No se pudo establecer una conexión con la base de datos ').': '.self::$connection->connect_error.'('.self::$connection->connect_errno.')');
 		}
-	
+
 		$this->connected = true;
 	}
-	
+
 	/**
 	 * Verifica si la conexion esta activa
 	 * @return MySQLConnection Este método se puede encadenar
 	 * @throws BadConnectionException
-	 */	
+	 */
 	public function checkConnection()
 	{
 		if (!$this->connected)
@@ -102,19 +103,19 @@ class MySQLConnection implements DBConnection{
 				throw new BadConnectionException(_('Ocurrió un error al verificar el estado activo de la conexión'));
 			}
 		}
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * Devuelve el recurso de conexión actual
 	 * @return mysqli
-	 */	
+	 */
 	public function getConnection()
 	{
 		return $this->connection;
 	}
-	
+
 	/**
 	 * Devuelve el manejador de la base de datos
 	 * @return MySQLManager
@@ -123,7 +124,7 @@ class MySQLConnection implements DBConnection{
 	{
 		return new MySQLManager($this);
 	}
-	
+
 }
 
 // Fin del archivo
